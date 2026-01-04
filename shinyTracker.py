cd976f4493c190f860eb26c919135aadfbdcf741
joshualukejewson <joshuajewson@icloud.com>
2026-01-04 19:59:38 +1000

    adds the fetch pokemon data function and creates a pokemon object with the name and shiny sprite.

--- shinyTracker.py
+++ shinyTracker.py
@@ -3,14 +3,28 @@
 # Made by Joshua Luke.
 
 import requests
+from PIL import Image
 
 class Pokemon:
     def __init__(self, name, sprite):
         self.name = name
         self.sprite = sprite
 
+    def __str__(self):
+        return f"{self.name} - {self.sprite}"
+    
+def fetch_pokemon_data(pokemon_name):
+    url = "https://pokeapi.co/api/v2/pokemon/{name}".format(name = pokemon_name)
+    print(url)
+    response = requests.get(url)
+    raw_data = response.json()
+
+    target_pokemon = Pokemon(raw_data["name"], raw_data["sprites"]["front_shiny"] )
+
+    #print(target_pokemon)
+
 def main():
-    pass
+    fetch_pokemon_data("bulbasaur")
 
 if __name__ == '__main__':
     main()

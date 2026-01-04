5141512a843d46f26765203026988b3885383915
joshualukejewson <joshuajewson@icloud.com>
2026-01-04 19:31:49 +1000

    adds the pokemon class and sets up the main function and request library.

---
+++ shinyTracker.py
@@ -0,0 +1,16 @@
+# shiny tracker with PokiApi integration, animated sprites,
+# pokemon search functionality and interactive GUI.
+# Made by Joshua Luke.
+
+import requests
+
+class Pokemon:
+    def __init__(self, name, sprite):
+        self.name = name
+        self.sprite = sprite
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()

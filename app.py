d6ff190f20af076ada5354d7cbf138cded370e44
joshualukejewson <joshuajewson@icloud.com>
2026-01-07 17:04:46 +1000

    created poketrack.py module handling pokemon related code with pokeapi handling.

--- app.py
+++ app.py
@@ -1,50 +1,32 @@
 """ 
-shiny tracker with PokiApi integration, animated sprites,
+Shiny tracker with Pokeapi integration, animated sprites,
 pokemon search functionality and interactive GUI.
 Made by Joshua Luke. 
 """
 
 import requests
 from flask import Flask, render_template, request
+import poketrack
 
 app = Flask(__name__)
 
-class Pokemon: 
-    def __init__(self, name : str, sprite : str, shiny: str):
-        self.name = name
-        self.sprite = sprite
-        self.shimy = shiny
-        self.count = 0
+"""
+Index Route that loads the main page, takes pokemon search input and displays the pokemon to the screen.
 
-    def increment(self):
-        self.count += 1
-
-    def get_count(self):
-        return self.count
-    
-    def reser_count(self):
-        self.count = 0
-        
-    def __str__(self):
-        return f"{self.name} - {self.sprite}"
-    
-def fetch_pokemon_data(pokemon_name : str) -> Pokemon:
-    url = "https://pokeapi.co/api/v2/pokemon/{name}".format(name = pokemon_name)
-    print(url)
-    response = requests.get(url)
-    raw_data = response.json()
-
-    return Pokemon(raw_data["name"].capitalize(), raw_data["sprites"]["front_default"], raw_data["sprites"]["front_shiny"])
-
+@param: None
+@return: flask.render_template() function.
+"""
 @app.route("/", methods=["GET", "POST"])
 def index():
 
     pokemon_search = ""
     pokemon = None
+
     if request.method == "POST":
         pokemon_search = request.form.get('pokemon_search')
         if pokemon_search:
-            pokemon = fetch_pokemon_data(str(pokemon_search).lower())
+            pokemon = poketrack.fetch_pokemon_data(str(pokemon_search).lower())
+
     return render_template("index.html", pokemon=pokemon)
 
 if __name__ == '__main__':
---
+++ poketrack.py
@@ -0,0 +1,28 @@
+import requests
+
+class Pokemon: 
+    def __init__(self, name : str, sprite : str, shiny: str):
+        self.name = name
+        self.sprite = sprite
+        self.shimy = shiny
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def get_count(self):
+        return self.count
+    
+    def reser_count(self):
+        self.count = 0
+        
+    def __str__(self):
+        return f"{self.name} - {self.sprite}"
+    
+def fetch_pokemon_data(pokemon_name : str) -> Pokemon:
+    url = "https://pokeapi.co/api/v2/pokemon/{name}".format(name = pokemon_name)
+    print(url)
+    response = requests.get(url)
+    raw_data = response.json()
+
+    return Pokemon(raw_data["name"].capitalize(), raw_data["sprites"]["front_default"], raw_data["sprites"]["front_shiny"])
